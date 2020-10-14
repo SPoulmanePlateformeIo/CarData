1906dedcdbde7e0e1a55e0e3b27c36de70ba5b73
SPoulmanePlateformeIo <sebastien.poulmane@laplateforme.io>
2020-10-14 15:19:34 +0200

    Fix Dash app for real

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
-from app import server
+from app import app, server
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd
 
 df = pd.read_csv('data/carData.csv')
